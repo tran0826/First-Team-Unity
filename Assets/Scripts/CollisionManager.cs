bee00e81134347085dba8ba33ede4992716c2a7b
tran0826 <sephirothkaito0826@yahoo.co.jp>
2019-09-03 03:25:01 +0900

    ManagerClass

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+
+    private List<EnemyBulletPair> enemyBulletCoList = new List<EnemyBulletPair>();
+
+    private DamageManager damageManager;
+
+    private void Awake()
+    {
+        damageManager = GameManager.Instance.damageManager;
+    }
+
+    public void UpdateByFrame()
+    {
+
+
+
+
+        ResetCoList();
+    }
+
+    private void ResetCoList()
+    {
+        enemyBulletCoList.Clear();
+    }
+
+    //敵と弾の衝突時に呼び出し
+    public void CollisionBulletToEnemy(EnemyController enemy)
+    {
+        EnemyBulletPair pair = new EnemyBulletPair(enemy);
+        enemyBulletCoList.Add(pair);
+    }
+}
+
+public class EnemyBulletPair
+{
+    public EnemyController enemy;
+//    public BulletBase bullet;
+
+    public EnemyBulletPair(EnemyController enemy)
+    {
+        this.enemy = enemy;
+    }
+}
---
+++ Assets/Scripts/DamageManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour
+{
+    private Queue<EnemyBulletPair> enemyBulletCoList = new Queue<EnemyBulletPair>();
+
+
+
+    private void Awake()
+    {
+
+    }
+
+
+    public void SetCoList(List<EnemyBulletPair> ebList)
+    {
+        foreach(var enemyBulletPair in ebList)
+        {
+            enemyBulletCoList.Enqueue(enemyBulletPair);
+        }
+    }
+
+    public void UpdateByFrame()
+    {
+        ExecEnemyBulletProcess();
+    }
+
+
+    private void ExecEnemyBulletProcess()
+    {
+        if (enemyBulletCoList.Count == 0) return;
+        foreach(EnemyBulletPair pair in enemyBulletCoList)
+        {
+
+
+
+        }
+        enemyBulletCoList.Clear();
+    }
+}
