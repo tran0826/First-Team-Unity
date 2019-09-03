1aac3919484a14ef2c0313bdfddcb8f2c4acf32e
momoharahara <48521552+momoharahara@users.noreply.github.com>
2019-09-03 23:51:54 +0900

    Create Bulletfile

---
+++ Assets/Scripts/Bullet/BulletBace.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public enum Group
+{
+    Player,
+    Enemy
+}
+
+public abstract class BulletBace : MonoBehaviour
+{
+    [SerializeField]
+    private int power;
+    [SerializeField]
+    protected float speed;
+    protected Vector3 direction;
+    private Renderer bulletRenderer;
+
+    [SerializeField] public Group group = Group.Enemy;
+    public int Power { get { return power; } protected set { power = value; } }
+
+    public virtual bool CanDestroyOnCollision { get; protected set; } = true;
+
+    //public PowerType PowerType { get; protected set; }
+
+    void Awake()
+    {
+        bulletRenderer = GetComponent<Renderer>();
+    }
+
+    public abstract void Move();
+
+    void Update()
+    {
+        Move();
+        UpdateRotation();
+
+    }
+    public abstract void Initialize(float ratio);
+
+    private void UpdateRotation()
+    {
+        if (Vector3.Angle(this.transform.up, direction) < 5f) return;
+
+        this.transform.rotation = Quaternion.FromToRotation(this.transform.up, direction);
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D collider)
+    {
+        /*
+        var player = collider.gameObject.GetComponent<PlayerController>();
+        if (player != null && group == Group.Enemy)
+        {
+            GameManager.Instance.collisionManager.CollisionBulletToPlayer(player, this);
+            return;
+        }
+        */
+
+        var enemy = collider.gameObject.GetComponent<EnemyController>();
+        if (enemy != null && group == Group.Player)
+        {
+            GameManager.Instance.collisionManager.CollisionBulletToEnemy(enemy);
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    //[SerializeField]
+    //private PowerType powerType;
+    [SerializeField]
+    private float maxInterval;
+    [SerializeField]
+    private float minInterval;
+    [SerializeField]
+    private int maxPower;
+
+    private double lastShootSec;
+    private float levelInterval;
+    private int level;
+    private bool isActive = false;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        lastShootSec = GameManager.Instance.timeManager.AbsoluteTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
