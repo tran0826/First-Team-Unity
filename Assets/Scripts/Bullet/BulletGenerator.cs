e0aec830dc60370de715355b0ae72158b214a4e6
momoharahara <48521552+momoharahara@users.noreply.github.com>
2019-09-05 19:11:05 +0900

    Create BulletGenerater

---
+++ Assets/Scripts/Bullet/BulletGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerator : MonoBehaviour
+{
+    private List<Transform[]> eachBulletPositions = new List<Transform[]>();
+    private GameObject targetEnemy;
+    private float interval;
+    private int power;
+    private float time;
+    [SerializeField]  BulletType bulletType;
+    [SerializeField]  public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interval = GameManager.Instance.playerManager.GetInterval();
+        power = GameManager.Instance.playerManager.GetPower();
+        time = 0;
+        bulletType=gameObject.GetComponent<TowerController>().bulletType;
+        targetEnemy = GameManager.Instance.enemyManager.NearestEnemy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += (float)GameManager.Instance.timeManager.DeltaTime();
+        if (time >= interval) {
+            time -= interval;
+            CreateBullet(bulletType);
+        }
+    }
+
+    public void CreateBullet(BulletType type)
+    {
+        
+        interval = GameManager.Instance.playerManager.GetInterval();
+        power = GameManager.Instance.playerManager.GetPower();
+        if (targetEnemy != null) {
+            targetEnemy = GameManager.Instance.enemyManager.NearestEnemy(this.gameObject);
+        }
+        var bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity).GetComponent<BulletBase>();
+
+    }
+}
--- Assets/Scripts/Tower/TowerController.cs
+++ Assets/Scripts/Tower/TowerController.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class TowerController : MonoBehaviour
 {
+    
+    public BulletType bulletType = BulletType.Fire;
+
+    [SerializeField] private BulletGenerator bulletGenerator;
+
+    private void Awake()
+    {
+        bulletGenerator = GetComponent<BulletGenerator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.Instance.towerManager.Born(gameObject);
+
     }
 
     // Update is called once per frame
