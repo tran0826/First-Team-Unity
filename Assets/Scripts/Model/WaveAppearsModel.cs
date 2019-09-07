327d8509127e449212f82195199d05d9f211dd27
tran0826 <sephirothkaito0826@yahoo.co.jp>
2019-09-08 02:51:22 +0900

    Tools

---
+++ Assets/Scripts/Model/WaveAppearsModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Model
+{
+    public class WaveAppearsModel
+    {
+        public List<WaveAppearModel> WaveAppearModels { get; private set; }
+
+        public WaveAppearsModel(List<WaveAppearModel> waveAppearModels)
+        {
+            this.WaveAppearModels = waveAppearModels;
+        }
+
+        public List<WaveAppearModel> SearchAppearModel(double elapsedTime)
+        {
+            List<WaveAppearModel> appearModels = new List<WaveAppearModel>();
+            foreach(var waveAppearModel in WaveAppearModels)
+            {
+                if (waveAppearModel.CanAppear(elapsedTime))
+                {
+                    appearModels.Add(waveAppearModel);
+                }
+            }
+            return appearModels;
+        }
+
+        public bool IsAllAppeard
+        {
+            get
+            {
+                return WaveAppearModels.All(model => model.IsAppeared);
+            }
+        }
+    }
+}
