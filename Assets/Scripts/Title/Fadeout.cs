243b69e7bbf12c6cc18ac659e1ec4345879b3f1f
tran0826 <sephirothkaito0826@yahoo.co.jp>
2019-10-28 07:22:39 +0900

    fadein

--- Assets/Scripts/Title/Fadeout.cs
+++ Assets/Scripts/Title/Fadeout.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Fadeout : MonoBehaviour
 {
-    float alpha = 0;
+    float alpha = 1;
     // Start is called before the first frame update
     void Awake()
     {
@@ -15,9 +15,15 @@ public class Fadeout : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (TitleManager.Instance.transFlag == true)
         {
             alpha += (float)TitleManager.Instance.timeManager.DeltaTime()/3.0f;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
+        }
+        else if(alpha>0)
+        {
+            alpha -= (float)TitleManager.Instance.timeManager.DeltaTime();
             gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
         }
     }
